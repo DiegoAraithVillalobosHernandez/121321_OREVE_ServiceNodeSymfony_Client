a5e9b44a3496e0c1921b5be6e65d8ebd04c67b46
CokerAlcocer <20203tn066@utez.edu.mx>
2021-12-07 21:22:10 -0600

    UPDATE 07/12/2021 21:22

--- serverNJS/src/keys.js
+++ serverNJS/src/keys.js
@@ -2,7 +2,7 @@ module.exports= {
     database:{
         host: "localhost",
         user: "root",
-        password: "ayad2923",
+        password: "",
         dateStrings: true,
         database: "oreve"
     }
